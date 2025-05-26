4a418d96ed7785ebac7e8ab12cb93c86d1624114
Varsha Awasthi <awasthivarsha0710@gmail.com>
2025-05-26 22:30:14 +0530

    fiv

--- src/api.jsx
+++ src/api.jsx
@@ -3,13 +3,13 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 export const fetchDestinations = async () => {
-  const response = await fetch(`${BASE_URL}/api/destinations`);
+  const response = await fetch(`${BASE_URL}/destinations`);
   if (!response.ok) throw new Error("Failed to fetch destinations");
   return response.json();
 };
 
 export const fetchPackages = async () => {
-  const response = await fetch(`${BASE_URL}/api/packages/top-selling`);
+  const response = await fetch(`${BASE_URL}/packages/top-selling`);
   if (!response.ok) throw new Error("Failed to fetch packages");
   return response.json();
 };
